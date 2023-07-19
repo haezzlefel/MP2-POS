9ea8fe682dd890308389b76da294e80a67549663
haezzle fel <zerrazozila@gmail.com>
2023-07-19 17:13:48 +0800

    modified:   css/style.css 	new file:   images /wlogo.png 	new file:   images /wlogo1.png 	modified:   index.html 	new file:   index2.html 	new file:   js/calculate.js 	new file:   js/dt.js 	new file:   new.css 	new file:   new.html 	modified:   pos.js

--- pos.js
+++ pos.js
@@ -13,48 +13,32 @@ function ordercart(itemname, itemprice){
     orderarray.push(itemname, itemprice);
 
     let ordersummary = document.getElementById('ordersummary');
-
     //Create LI tag
     const orderitem = document.createElement('li');
-    orderitem.className = 'd-flex justify-content-between align-items-center p-1';
-
+    orderitem.className = 'd-flex justify-content-between align-items-center text-center p-1';
     //Create span for red color
     const orderitempricespan = document.createElement('span');
-
     //Created item details to node
     const orderitemname = document.createTextNode(itemname);
     const orderitemprice = document.createTextNode(' P ' + itemprice);
-
     // Price color red
-    orderitempricespan.className = 'text-secondary';
-
+    orderitempricespan.className = 'text-danger';
     orderitempricespan.appendChild(orderitemprice);
-
     const deletebutton = document.createElement('button');
     const deletebuttontext = document.createTextNode('Remove');
-
     deletebutton .setAttribute('onclick', 'deleteItem('+i+', this)');
-
     deletebutton.appendChild(deletebuttontext);
-
-    deletebutton.className = 'btn btn-dark rounded';
-
+    deletebutton.className = 'btn btn-dark btn-sm rounded-pill';
     //Append item to LI
     orderitem.appendChild(orderitemname);
-
     orderitem.appendChild(orderitempricespan);
-
     orderitem.appendChild(deletebutton);
-
     //Append the LI tag to parent ordersummary
     ordersummary.appendChild(orderitem);
-
     totalitems(); 
     totalcost();
-    
     i++;
-
-    console.log(orderidarray);
+    validateCheckoutBtn();
 };
 
 function totalitems(){
@@ -66,67 +50,49 @@ function totalcost(){
         document.getElementById('totalcost').innerText = 0;
     } else {
         document.getElementById('totalcost').innerText = orderpricearray.reduce(sumarray).toFixed(2);
-        function sumarray(total, num) {
-        return total + num;
+        document.getElementById('totalamt').value = orderpricearray.reduce(sumarray).toFixed(2);
+        function sumarray(total, sum) {
+        return total + sum;
         };
-      }
     }
+};
 
 function cartClear(){
     let ordersummary = document.getElementById('ordersummary');
+    document.getElementById('totalamt').value = 0;
     ordersummary.innerHTML = '';
     orderitemsarray.length = 0;
     orderpricearray.length = 0;
     orderarray.length = 0;
     orderidarray.length = 0;
     i = 0;
-
     totalitems();
     totalcost();
-}
+    validateCheckoutBtn();
+};
 
 function deleteItem(orderid, button) {
     const indexnum = orderidarray.indexOf(orderid);
     orderidarray.splice(indexnum, 1);
     orderitemsarray.splice(indexnum, 1);
     orderpricearray.splice(indexnum, 1);
-
     totalitems(); 
     totalcost();
-
     ordersummary.removeChild(button.parentElement);
+    validateCheckoutBtn();
 };
 
+function transactionNumber() {
+    let transnum = Math.floor(Math.random() * 10000)
+    document.getElementById('transnum').innerHTML = transnum;
+    document.getElementById('modaltransnum').innerHTML = transnum;
+    document.getElementById('receipttransnum').innerHTML = transnum;
+};
+transactionNumber();
 
-function display_cdt() {
-    var x = new Date()
-    var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
-    hours = x.getHours( ) % 12;
-    hours = hours ? hours : 12;
-    hours=hours.toString().length==1? 0+hours.toString() : hours;
-
-    var minutes=x.getMinutes().toString()
-    minutes=minutes.length==1 ? 0+minutes : minutes;
-    
-    var seconds=x.getSeconds().toString()
-    seconds=seconds.length==1 ? 0+seconds : seconds;
-
-    var month=(x.getMonth() +1).toString();
-    month=month.length==1 ? 0+month : month;
-
-    var dt=x.getDate().toString();
-    dt=dt.length==1 ? 0+dt : dt;
-
-    var x1=month + "/" + dt + "/" + x.getFullYear(); 
-    x1 = x1 + " - " +  hours + ":" +  minutes + ":" +  seconds + " " + ampm;
-    document.getElementById('cdt').innerHTML = x1;
-    display_c7();
+function validateCheckoutBtn() {
+    document.getElementById('checkout').disabled = true;
+    if(orderitemsarray.length > 0){
+        document.getElementById('checkout').disabled = false;
     }
-
-function display_c7(){
-    var refresh=1000; // Refresh rate in milli seconds
-    mytime=setTimeout('display_cdt()',refresh)
-
-}
-document.getElementById('cdt').innerHTML = display_c7();
-
+};
